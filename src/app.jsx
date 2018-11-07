f856a39492db923d68810e03931f78fe93eed556
Kaccady <z-rz@mail.com>
2018-11-07 17:40:00 +0300

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import 'react-select/dist/react-select.css';
+
+render(
+<div>
+    <p>Place your application here</p>
+</div>, document.getElementById('app-root'));
